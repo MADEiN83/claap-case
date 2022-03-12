ad40544aa1ce8283bf14fe6523aa5a832d4751b7
Anthony DI STEFANO <Anthony.Distefano@hotmail.fr>
2022-03-12 14:09:08 +0100

    feat: update typo

--- src/components/multi-select/components/new-item/index.tsx
+++ src/components/multi-select/components/new-item/index.tsx
@@ -4,7 +4,7 @@ import { Text } from "@chakra-ui/react";
 
 const NewItem: React.FC = (props) => {
   const { children } = props;
-  return <Text>New: {children}</Text>;
+  return <Text>New email: {children}</Text>;
 };
 
 export default NewItem;
